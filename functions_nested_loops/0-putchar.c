bec8384eda1acbf0adf745c3b182761ade60ef92
Buiscy <Aaron.regterschot@gmail.com>
2026-03-05 15:03:21 +0000

    Im confused

---
+++ functions_nested_loops/0-putchar.c
@@ -0,0 +1,20 @@
+#include "main.h"
+/**
+ *This Header specifies to be linked to a specific header within the directory with its own commands
+ *
+ * the following file prints "_putchar" letter by letter, but as one world
+ * this is going to suck
+ */
+int main(void)
+{
+	_putchar('_');
+	_putchar('p');
+	_putchar('u');
+	_putchar('t');
+	_putchar('c');
+	_putchar('h');
+	_putchar('a');
+	_putchar('r');
+	_putchar('\n');
+	return (0);
+}
---
+++ functions_nested_loops/README.md
@@ -0,0 +1,4 @@
+In this File I'm going to explain this mess to myself.
+So in 0-putchar.c:
+    This is where I start using my own new headers. The #include here is #include "main.h" - Its in qoutes as its specifying a non-standard libary.
+    
