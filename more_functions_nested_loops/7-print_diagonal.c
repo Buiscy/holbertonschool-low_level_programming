1dd9a7bf69e4d1be4ef4ea1ef775a6a59f7a1404
Buiscy <Aaron.regterschot@gmail.com>
2026-04-12 15:41:48 +0000

    pushing through the stress

--- more_functions_nested_loops/7-print_diagonal.c
+++ more_functions_nested_loops/7-print_diagonal.c
@@ -1,4 +1,20 @@
 #include "main.h"
 
 void print_diagonal(int n)
-
+{ 
+	if (n < 0)
+	{
+		_putchar('\n');
+	}
+	else 
+	{
+		int i = 0;
+		while ( i <= n)
+		{
+			_putchar(32);
+			i++;
+		}
+		_putchar(92);
+		_putchar(10);
+	}
+}
