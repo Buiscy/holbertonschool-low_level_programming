b6a4a0c6315bcc652fbd5ca95f1724f2e75dc3ed
Buiscy <Aaron.regterschot@gmail.com>
2026-04-14 13:20:29 +0000

    fixing my git

---
+++ pointers_arrays_strings/7-puts_half.c
@@ -0,0 +1,20 @@
+#include "main.h"
+
+int length (char *s);
+
+void puts_half(char *str)
+{
+	int i = length(b);
+	while (1)
+
+}
+
+int length (char *s)
+{
+	int b = 0;
+	while (s[i] != '\0')
+	{
+		i++
+	}
+	return (b);
+}
