979e7cad89e13e423edd1186631bee3f9542d78a
Buiscy <aaron.regterschot@gmail.com>
2026-07-12 21:10:28 +1000

    fixes

--- more_functions_nested_loops/8-print_square.c
+++ more_functions_nested_loops/8-print_square.c
@@ -3,17 +3,18 @@
 void print_square(int size)
 {
 	if (size < 1)
-	{ 
+	{
 		_putchar(10);
 	}
 	else
 	{
 
 		int i;
+		int j;
 
-		for ( i = 0; i < size; i++)
+		for (i = 0; i < size; i++)
 		{
-			int j;
+
 
 			for (j = 0; j < size; j++)
 			{
