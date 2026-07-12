7062e1d22eb92814ed748b41b24733d27403be98
Buiscy <aaron.regterschot@gmail.com>
2026-07-12 22:37:48 +1000

    fixes

--- simple-calculator/calculator.c
+++ simple-calculator/calculator.c
@@ -2,8 +2,8 @@
 
 int main (void)
 {
-    printf("Simple Calculator\n1) Add\n2) Subtract\n3) Multiply\n4) Divide\n0) Quit");
-    printf("Choice: 0");
-    printf("Bye!");
+    printf("Simple Calculator\n1) Add\n2) Subtract\n3) Multiply\n4) Divide\n0) Quit\n");
+    printf("Choice: 0\n");
+    printf("Bye!\n");
     return (0);
 }
