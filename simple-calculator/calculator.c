7ccd6946403a9c31c679ddc591a2522b8b660b22
Buiscy <aaron.regterschot@gmail.com>
2026-07-12 22:37:07 +1000

    Starting calc

--- simple-calculator/calculator.c
+++ simple-calculator/calculator.c
@@ -1,4 +1,9 @@
 #include <stdio.h>
 
 int main (void)
-    printf
+{
+    printf("Simple Calculator\n1) Add\n2) Subtract\n3) Multiply\n4) Divide\n0) Quit");
+    printf("Choice: 0");
+    printf("Bye!");
+    return (0);
+}
