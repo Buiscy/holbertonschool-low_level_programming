46bd9aaf03f391b807aafaafe7ac67e97a1f1f7c
Buiscy <aaron.regterschot@gmail.com>
2026-07-12 20:48:45 +1000

    done?

--- more_functions_nested_loops/10-print_triangle.c
+++ more_functions_nested_loops/10-print_triangle.c
@@ -5,6 +5,7 @@ void print_triangle(int size)
 {
     int i;
     int b;
+    int space;
     if (size <= 0)
     {
         _putchar('\n');
@@ -13,6 +14,10 @@ void print_triangle(int size)
 
         for (i = 0; i < size; i++)
         {
+	        for (space = 0; space < size - i - 1; space++)
+	        {
+		        _putchar(' ');
+	        }
             for (b = 0; b <= i; b++)
             {
                 _putchar('#');
