2cdf0fb48ddb11a241c15916a57460e071528d69
Buiscy <aaron.regterschot@gmail.com>
2026-07-13 00:07:08 +1000

    Done 7!

--- functions_nested_loops/7-print_last_digit.c
+++ functions_nested_loops/7-print_last_digit.c
@@ -7,9 +7,19 @@ int print_last_digit(int n)
     if (n > 10)
     {
         i = (n % 10);
+        _putchar (i + '0');
+        return (i);
+    }
+    else if (n <= -1)
+    {
+        i = (-n % 10);
+        _putchar (i + '0');
         return (i);
     }
     else
+    {
+        _putchar (n + '0');
         return (n);
-
+    }
+    return (0);
 }
