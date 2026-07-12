49faacfd8f0edbbe5b16c7aba9d2962629c4c177
Buiscy <aaron.regterschot@gmail.com>
2026-07-12 23:38:52 +1000

    Fixing overcomplicated code?

--- functions_nested_loops/0-putchar.c
+++ functions_nested_loops/0-putchar.c
@@ -1,19 +1,23 @@
 #include "main.h"
 /**
- *This Header specifies to be linked to a specific header within the directory with its own commands
- *
+ *This Header specifies to be linked to a
+ * specific header within the directory with its own commands
  * the following file prints "_putchar" letter by letter, but as one world
  * this is going to suck
  */
 int main(void)
 {
-int i = 0;
-	char s[]="_putchar";
-		{
-		for (i=0; s[i]!='\0';i++)
-		_putchar(s[i]);
-
-		}
-	_putchar('\n');
-	return (0);
+	printf("_putchar")
 }
+
+/*
+*int i = 0;
+*	char s[] ="_putchar";
+*		{
+*		for (i=0; s[i]!='\0';i++)
+*		_putchar(s[i]);
+*
+*		}
+*	_putchar('\n');
+*	return (0);
+*/
