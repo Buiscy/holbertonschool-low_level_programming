dcdf535ecb6defc4d00e75ba325de95e56391c7d
Buiscy <aaron.regterschot@gmail.com>
2026-07-22 11:39:50 +1000

    Fixing up 2-str

--- malloc_free/2-str_concat.c
+++ malloc_free/2-str_concat.c
@@ -24,12 +24,12 @@ char *str_concat(char *s1, char *s2)
 
 	i = 0, j = 0;
 
-	while (s1[i] !=  '\0')
+	while (s1 !=  NULL && s1[i] !=  '\0')
 	{
 		s3[i] = s1[i];
 		i++;
 	}
-	while (s2[j] != '\0')
+	while (s2 !=  NULL && s2[j] !=  '\0')
 	{
 		s3[i] = s2[j];
 		i++;
