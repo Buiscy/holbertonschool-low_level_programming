ea82b72365e2fa914c43a2bab231c0affd5d7acc
Buiscy <aaron.regterschot@gmail.com>
2026-08-08 16:59:23 +0000

    Found some time to do a bit of work

--- variadic_functions/0-sum_them_all.c
+++ variadic_functions/0-sum_them_all.c
@@ -1,7 +1,18 @@
-#include <stdio.h>
+#include <stdarg.h>
 #include "variadic_functions.h"
 
 int sum_them_all(const unsigned int n, ...)
 {
+    va_list ap;
+    unsigned int total = 0;
+    unsigned int i;
+    va_start (ap, n);
 
+
+    for (i = 0; i < n; i++)
+        total += va_arg(ap, int);
+    
+    va_end(ap);
+    return (total);
+    
 }
