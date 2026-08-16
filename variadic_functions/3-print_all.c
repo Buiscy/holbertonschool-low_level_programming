39b0258eae2abdd59685677faaae80b9e006d6c0
Buiscy <aaron.regterschot@gmail.com>
2026-08-16 19:21:41 +1000

    Push date

--- variadic_functions/3-print_all.c
+++ variadic_functions/3-print_all.c
@@ -1,18 +1,53 @@
 #include <stdio.h>
 #include <stdarg.h>
-#include <stdlib.h>
 #include "variadic_functions.h"
 
 void print_all(const char * const format, ...)
 {
-    int count;
-    va_list ap;
-    while (*format != '/0')
-        {
-            
-        }
-    va_end(ap);
-    return (count);
+	const char *p;
+	const char *s;
+	va_list ap;
+	const char *separator;
+	
+	separator = "";
+	p = format;
+	va_start (ap, format);
 
+	while (p != NULL && *p != '\0')
+		{
+			switch(*p)
+			{
 
+				case 'c':
+					printf("%s%c", separator, va_arg(ap, int));
+					separator = ", ";
+					break;
+				
+				case 'i':
+					printf("%s%i", separator, va_arg(ap, int));
+					separator = ", ";
+					break;
+
+				case 'f':
+					printf("%s%f", separator, va_arg(ap, double));
+					separator = ", ";
+					break;
+
+				case 's':
+					s = va_arg(ap, char *);
+
+					if  (s == NULL)
+						s = "(nil)";
+
+					printf("%s%s", separator, s);
+					separator = ", ";
+					break;
+
+			}
+
+			p++;
+
+		}
+	va_end(ap);
+	printf("\n");
 }
